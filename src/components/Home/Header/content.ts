4d27d404c784e9298b0e3bb9d9092aea5d4845bc
M Ahmed Mushtaq <mahmedmushtaq296@gmail.com>
2022-01-31 08:36:59 +0500

    updated package.json file

--- src/components/Home/Header/content.ts
+++ src/components/Home/Header/content.ts
@@ -1,6 +1,6 @@
 export const content = {
   primaryHeading: "Hi, I'am Ahmed",
   secondaryHeading:"Full Stack Developer",
-  description:" Highly expert in full stack web development by using React, React native, Next, node, TypeScript, GraphQL, Blockchain and web3",
+  description:" Highly expert in full stack web development by using React, React native, Next, node, TypeScript, GraphQL, Blockchain, web3, wordpress, and shopify",
   
 };
