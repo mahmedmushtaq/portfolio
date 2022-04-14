9aca8b185d51a70940175190dd417f392a478cef
M Ahmed Mushtaq <mahmedmushtaq296@gmail.com>
2022-04-14 15:15:17 +0500

    update qualifications and minor updates in testimonials

--- src/components/Home/Qualifications/list.ts
+++ src/components/Home/Qualifications/list.ts
@@ -1,34 +1,40 @@
 export const qualificationsList = [
   {
-    id: 1,
+    id: "smodin-io",
+    heading: "Full Stack developer at Smodin.io",
+    date: "Feb 2022 - Present",
+    technologiesList: "Technologies which I am using- Next, GCP, Firebase, MUI",
+  },
+  {
+    id: "foreseer-co",
     heading: "Front End developer at Foreseer.co ( Freelance )",
     date: "Dec 2021- Jan 2022",
     technologiesList:
       "Technologies which I used:- React, React Native, AWS amplify, dynamoDB",
   },
   {
-    id: 2,
+    id: "actual-ly",
     heading: " Full-stack developer at Actual.ly ( Contract )",
     date: "Mar 2021- Sep 2021",
     technologiesList:
       "Technologies which I used:- React, AWS serverless, dynamoDB, Node, Twilio",
   },
   {
-    id: 3,
+    id: "upwork-freelance",
     heading: "Freelancer at Upwork ( Freelance, Contract )",
     date: "Jan 2020- Feb 2021",
     technologiesList:
       "Technologies which I used:- TypeScript, React, React Native, Node.js, MondoDB, MySql, Next.js, Testing,  GraphQL, AWS ( amplify, serverless, dynamodb, kinesis video streaming ) ",
   },
   {
-    id: 4,
+    id: "orthiya-tec",
     heading: "Full-stack developer at orthiya technology ( Contract )",
     date: "May 2020 - Aug 2020",
     technologiesList:
       "Technologies which I used:- React, Node.js, MongoDB, GraphQL, Website Scrapping, Laravel, MySql, Next.js, Testing",
   },
   {
-    id: 5,
+    id: "contest-app",
     heading: "Full-stack developer at Contest",
     date: "Sep 2017 - June 2019",
     technologiesList:
--- src/components/Home/Testimonials/list.ts
+++ src/components/Home/Testimonials/list.ts
@@ -1,10 +1,12 @@
+import ChangWookPic from "../../../common/assets/client_chang.jpg";
+import SaadPic from "../../../common/assets/client_saad.jpg";
+
 export const testimonialsReview = [
   {
     id: 1,
     clientName: "Chang Wook Lee",
     companyName: "Foreseer.co",
-    imgSrc:
-      "https://media-exp1.licdn.com/dms/image/C5603AQE5fbcgncIXmw/profile-displayphoto-shrink_100_100/0/1517465618647?e=1648684800&v=beta&t=W3vqpjP_DBuf51L2UlwM009xES-KvHS4PZkw7oZJfNk",
+    imgSrc: ChangWookPic,
     review:
       " Ahmed was very quick in understanding and executing my requirements. I would highly recommend him for any of your development needs.",
   },
@@ -12,8 +14,7 @@ export const testimonialsReview = [
     id: 2,
     clientName: "Saad Malik",
     companyName: "Orthiya Technology",
-    imgSrc:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQFtn6uY-ih3ZQ/profile-displayphoto-shrink_400_400/0/1626019521521?e=1648684800&v=beta&t=FLoaEXPhXfVlx9ZquXmwQjNoGU2dX_x1WSB4ruKpY3U",
+    imgSrc: SaadPic,
     review:
       "Ahmed is a highly motivated individual. I appreciate his work ethics. His skills as a full stack developer are exceptional.",
   },
